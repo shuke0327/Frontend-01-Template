62dc5192d9a4662d78eabb3997127c6e843f6647
shuke0327 <jingkai64@gmail.com>
2020-05-20 18:19:16 +0800

    finish parser for style

--- week06/parser.js
+++ week06/parser.js
@@ -18,12 +18,97 @@ function addCSSRules(text) {
     rules.push(...ast.stylesheet.rules);
 }
 
+function match(element, selector) {
+    if (!selector || !element.attributes)
+        return false;
+    if (selector.charAt(0) === "#") {
+        var attr = element.attributes.filter(attr => attr.name === "id")[0]
+        if (attr && attr.value === selector.replace("#", ''))
+            return true;
+    } else if (selector.charAt(0) === ".") {
+        var attr = element.attributes.filter(attr => attr.name === "class")[0]
+        if (attr && attr.value === selector.replace(".", ''))
+            return true;
+    } else {
+        if (element.tagName === selector) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function specificity(selector) {
+    var p = [0, 0, 0, 0];
+    var selectorParts = selector.split(" ");
+    for (var part of selectorParts) {
+        if (part.charAt(0) === '#') {
+            p[1] += 1;
+        }
+        else if (part.charAt(0) === ".") {
+            p[2] += 1;
+        } else {
+            p[3] += 1;
+        }
+    }
+    return p;
+}
+
+function compare(sp1, sp2) {
+    if (sp1[0] - sp2[0]) {
+        return sp1[0] - sp2[0];
+    }
+    if (sp1[1] - sp2[1]) {
+        return sp1[1] - sp2[1];
+    }
+    if (sp1[2] - sp2[2]) {
+        return sp1[2] - sp2[2];
+    }
+    return sp1[3] - sp1[3];
+}
+
 function computeCSS(element) {
     // use slice to copy the current status;
     // use reverse
     var elements = stack.slice().reverse();
-    console.log('----', element);
-    console.log('====', elements);
+    if (!element.computedStyle) {
+        element.computedStyle = {};
+    }
+
+    for (let rule of rules) {
+        var selectorParts = rule.selectors[0].split(" ").reverse();
+        if (!match(element, selectorParts[0])) {
+            continue;
+        }
+        let matched = false;
+        // used for selector point
+        var j = 1;
+        for (var i = 0; i < elements.length; i++) {
+            if (match(elements[i], selectorParts[j])) {
+                j++;
+            }
+        }
+        if (j >= selectorParts.length) {
+            matched = true;
+        }
+
+        if (matched) {
+            // console.log("Element", element, "matched rule", rule);
+            var sp = specificity(rule.selectors[0]);
+            var computedStyle = element.computedStyle;
+            for (var declaration of rule.declarations) {
+                if (!computedStyle[declaration.property])
+                    computedStyle[declaration.property] = {}
+                if (!computedStyle[declaration.property].specificity) {
+                    computedStyle[declaration.property].value = declaration.value
+                    computedStyle[declaration.property].specificity = sp
+                } else if (compare(computedStyle[declaration.property].specificity, sp) < 0) {
+                    computedStyle[declaration.property].value = declaration.value
+                    computedStyle[declaration.property].specificity = sp
+                }
+            }
+            console.log(computedStyle)
+        }
+    }
 
 }
 
